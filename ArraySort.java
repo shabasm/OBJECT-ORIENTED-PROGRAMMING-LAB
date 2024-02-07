6a24bc2ea3c924bdc1cae52f3aed76531d6c6d30
Shabas Muhammed <123447887+shabasm@users.noreply.github.com>
2024-02-07 23:19:24 +0530

    Add files via upload

---
+++ ArraySort.java
@@ -0,0 +1,78 @@
+import java.util.Scanner;
+
+public class ArraySort {
+    public static void main(String[] args) {
+        try (Scanner input = new Scanner(System.in)) {
+            int temp, j, i;
+
+            System.out.print("Enter array size:");
+            int size = input.nextInt();
+            int a[] = new int[size];
+
+            System.out.println("Enter array elements");
+            for (i = 0; i < size; i++) {
+                System.out.print("At index " + i + ":");
+                a[i] = input.nextInt();
+            }
+
+            System.out.print("Array is:");
+            for (i = 0; i < size; i++) {
+                System.out.print(a[i] + " ");
+            }
+            for (i = 0; i < size; i++) {
+                for (j = 0; j <= i; j++) {
+                    if (a[i] < a[j]) {
+                        temp = a[j];
+                        a[j] = a[i];
+                        a[i] = temp;
+                    }
+                }
+            }
+
+            System.out.print("\nSorted array is:");
+            for (i = 0; i < size; i++) {
+                System.out.print(a[i] + " ");
+            }
+        }
+    }
+}
+
+// import java.util.Scanner;
+
+// public class ArraySort {
+// public static void main(String[] args) {
+// Scanner input = new Scanner(System.in);
+// int temp, j, i;
+
+// System.out.print("Enter array size:");
+// int size = input.nextInt();
+// int a[] = new int[size];
+
+// System.out.println("Enter array elements");
+// for(i = 0;i<size;i++){
+// System.out.print("At index " + i + ":");
+// a[i] = input.nextInt();
+// }
+
+// System.out.print("Array is:");
+// for(i = 0;i<size;i++){
+// System.out.print(a[i] + " ");
+// }
+// for(i = 0;i < size;i++){
+// for(j=0;j<=i;j++){
+// if(a[i] < a[j]){
+// temp = a[j];
+// a[j] = a[i];
+// a[i] = temp;
+// }
+// }
+// }
+
+// System.out.print("\nSorted array is:");
+// for(i = 0;i<size;i++){
+// System.out.print(a[i] + " ");
+// }
+
+// input.close();
+// }
+// }
