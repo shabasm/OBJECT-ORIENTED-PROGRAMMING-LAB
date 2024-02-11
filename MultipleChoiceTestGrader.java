97d80a534f381fffb7b8a04d0f5e20d090316148
Shabas Muhammed <123447887+shabasm@users.noreply.github.com>
2024-02-11 12:25:30 +0530

    Add files via upload

---
+++ MultipleChoiceTestGrader.java
@@ -0,0 +1,33 @@
+public class MultipleChoiceTestGrader {
+    public static void main(String[] args) {
+        int i, j, m = 0;
+        char stud[][] = {
+                { 'D', 'C', 'A', 'B' },
+                { 'C', 'B', 'D', 'A' },
+                { 'B', 'C', 'D', 'A' }
+        };
+        char ans[] = { 'C', 'B', 'D', 'A' };
+
+        System.out.println("Student Answers-");
+        for (i = 0; i < stud.length; i++) {
+            for (j = 0; j < stud[i].length; j++) {
+                System.out.print(stud[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println("Answer key-");
+        for (i = 0; i < ans.length; i++) {
+            System.out.print(ans[i] + " ");
+        }
+        System.out.println();
+        for (i = 0; i < stud.length; i++) {
+            for (j = 0; j < stud[i].length; j++) {
+                if (stud[i][j] == ans[j]) {
+                    m = m + 1;
+                }
+            }
+            System.out.println("Mark of student " + (i+1) + ":" + m);
+            m = 0;
+        }
+    }
+}
