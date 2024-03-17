2f910ca7618d6041c16ba48bf55d52b16688c4dd
Shabas Muhammed <123447887+shabasm@users.noreply.github.com>
2024-03-17 19:24:11 +0530

    Update Employee.java

--- Employee.java
+++ Employee.java
@@ -1,75 +1,69 @@
 import java.util.Scanner;
 
 class EmployeeDatabase {
-    Scanner scanner = new Scanner(System.in);
-    
-    int i = 0;
-    int[] empIdArray = new int[5];
-    String[] empNameArray = new String[5];
-    float[] empSalaryArray = new float[5];
+    int empId;
+    String empName;
+    float empSalary;
 
-    void insertDetail() {
-        if (i < empIdArray.length) {
-            System.out.println("Enter ID:");
-            empIdArray[i] = scanner.nextInt();
-            System.out.println("Enter Name:");
-            empNameArray[i] = scanner.next();
-            System.out.println("Enter Salary:");
-            empSalaryArray[i] = scanner.nextFloat();
-            i += 1;
-        } else {
-            System.out.println("Array is full. Cannot insert more employees.");
-        }
+    public EmployeeDatabase(int empId, String empName, float empSalary) {
+        this.empId = empId;
+        this.empName = empName;
+        this.empSalary = empSalary;
     }
 
-    void searchDetail() {
-        System.out.println("Enter ID to search:");
-        int searchId = scanner.nextInt();
-        
-        boolean found = false;
-        
-        for (int j = 0; j < i; j++) {
-            if (empIdArray[j] == searchId) {
-                System.out.println("Employee ID: " + empIdArray[j]);
-                System.out.println("Employee Name: " + empNameArray[j]);
-                System.out.println("Employee Salary: " + empSalaryArray[j]);
-                found = true;
-                break;
-            }
-        }
-        if (!found) {
-            System.out.println("Employee with ID " + searchId + " not found.");
-        }
+    void displayDetails() {
+        System.out.println("Employee ID: " + empId);
+        System.out.println("Employee Name: " + empName);
+        System.out.println("Employee Salary: " + empSalary);
     }
 }
 
 public class Employee {
     public static void main(String[] args) {
-        try (Scanner scanner = new Scanner(System.in);) {
-            
-            int choice, wish;
-            
-            EmployeeDatabase empDetail = new EmployeeDatabase();
+        Scanner scanner = new Scanner(System.in);
+        int choice, wish, empId, i = 0;
+        String empName;
+        float empSalary;
+        EmployeeDatabase[] empDetail = new EmployeeDatabase[5];
 
-            do {
-                System.out.println("------SELECT------\n1. Insert employee details\n2. Search employee details");
-                System.out.print("Enter your choice:");
-                choice = scanner.nextInt();
+        do {
+            System.out.println("------SELECT------\n1. Insert employee details\n2. Search employee details");
+            System.out.print("Enter your choice:");
+            choice = scanner.nextInt();
 
-                switch (choice) {
-                    case 1:
-                        empDetail.insertDetail();
-                        break;
-                    case 2:
-                        empDetail.searchDetail();
-                        break;
-                    default:
-                        System.out.println("Invalid choice.");
-                        break;
-                }
-                System.out.println("Do you wish to continue?(1/0)");
-                wish = scanner.nextInt();
-            } while (wish == 1);
-        }
+            switch (choice) {
+                case 1:
+                    if (i < empDetail.length) {
+                        System.out.println("Enter ID:");
+                        empId = scanner.nextInt();
+                        System.out.println("Enter Name:");
+                        empName = scanner.next();
+                        System.out.println("Enter Salary:");
+                        empSalary = scanner.nextFloat();
+                        empDetail[i] = new EmployeeDatabase(empId, empName, empSalary);
+                        i++;
+                    } else
+                        System.out.println("Cannot enter more.");
+                    break;
+                case 2:
+                    System.out.println("Enter ID to search:");
+                    int searchId = scanner.nextInt();
+                    boolean found = false;
+                    for (int j = 0; j < i; j++) {
+                        if (empDetail[j].empId == searchId) {
+                            empDetail[j].displayDetails();
+                            found = true;
+                            break;
+                        }
+                    }
+                    if (!found)
+                        System.out.println("Employee not found.");
+                    break;
+            }
+            System.out.println("Do you wish to continue?(1/0)");
+            wish = scanner.nextInt();
+        } while (wish == 1);
+        
+        scanner.close();
     }
 }
